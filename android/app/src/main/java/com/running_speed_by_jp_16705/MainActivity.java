bcffd264654c58f14fae701c9f94e1994394e633
Crowdbotics <team@crowdbotics.com>
2020-05-07 00:34:24 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/running_speed_by_jp_16705/MainActivity.java
@@ -0,0 +1,15 @@
+package com.running_speed_by_jp_16705;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "running_speed_by_jp_16705";
+    }
+}
